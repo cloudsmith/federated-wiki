33d32368bd418e3794f2f8bd443f7d0141074af2
mruzicka <michal.ruza@gmail.com>
2012-05-02 11:08:00 +0000

    add rdoc as a common dependency

--- modules/rubygems19/manifests/common_dependencies.pp
+++ modules/rubygems19/manifests/common_dependencies.pp
@@ -2,4 +2,9 @@ class rubygems19::common_dependencies {
 	package { ['gcc', 'gcc-c++', 'make', 'ruby19-devel']:
 		ensure => installed,
 	}
+
+	package { 'rdoc':
+		ensure => installed,
+		provider => gem19,
+	}
 }
